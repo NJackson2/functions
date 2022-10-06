ce44695983139a208aba766c7c4c16767d12f970
Nicholas Jackson <nickdjackson@Nicks-MacBook-Air.local>
2022-10-06 14:38:16 -0400

    added minus

--- function.js
+++ function.js
@@ -2,9 +2,13 @@
 //     return a + b
 // }
 
-
+const subtract = require("./minus")
 
 const addTwo = require("./external.js")
 
-const sum = addTwo(3,4)
-console.log(sum)
+// const sum = addTwo(3,4)
+// console.log(sum)
+
+
+const total = subtract(10,5)
+console.log(total)
